f7c7affb54331f60385b35d048d18333c63f4fd1
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-05-08 14:44:17 +0200

    feat: esercizio 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,10 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+
+names.forEach((currElem) => {
+
+    return console.log(currElem)
+
+})
